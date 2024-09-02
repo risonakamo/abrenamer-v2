386e5f636b9c0305d7426036a9b0a72aff9f5804
risonakamo <hmykanae@gmail.com>
2024-09-02 12:38:13 -0400

    implementing main renamer func moving test into test file

--- app/lib/renamer.ts
+++ app/lib/renamer.ts
@@ -1,10 +1,11 @@
 // renamer lib funcs
 
 import _ from "lodash";
+import {join} from "path";
 
 import {RenamerHandlebars} from "@/lib/RenamerHandlebars";
 
-function renameGroupedItems(
+export function renameGroupedItems(
     items:GroupedPaths,
     groupRenameRule:string,
     itemRenameRule:string,
@@ -12,39 +13,63 @@ function renameGroupedItems(
     renameMode:RenameMode,
 ):void
 {
-
-}
-
-export function test_applyRenameRule():void
-{
-    const paths:string[]=[
-        "a.png",
-        "asdas.jpg",
-        "huh",
-        "something.txt",
-        "something.jpg",
-    ];
-
     const renamer:RenamerHandlebars=new RenamerHandlebars();
 
-    var output:string[]=renamer.compileRule(
-        paths,
-        "{{inc 1}}",
-    );
 
-    console.log(output);
+    // --- generating names for groups
+    var groupNames:string[]=_.map(items,(__:string[],groupI:number):string=>{
+        return `${groupI+1}`;
+    });
 
-    output=renamer.compileRule(
-        paths,
-        "{{inc 12}}-{{random}}",
-    );
+    groupNames=renamer.compileRule(groupNames,groupRenameRule);
 
-    console.log(output);
+    // convert group names into paths based on output path
+    const groupPaths:string[]=_.map(groupNames,(groupName:string):string=>{
+        return join(outputDir,groupName);
+    });
 
-    output=renamer.compileRule(
-        paths,
-        "{{random}} {{filename}}",
-    );
+    if (_.uniq(groupPaths).length<groupPaths.length)
+    {
+        console.error("failed to rename items: duplicate group names in rename result");
+        console.error("group names:",groupNames);
+        throw "duplicate group names";
+    }
 
-    console.log(output);
+    if (groupPaths.length!=items.length)
+    {
+        console.error("generated group paths should match amount "
+            +"of rename groups provided, but doesn't");
+        console.error("group paths:",groupPaths);
+        console.error("rename item groups:",items);
+        throw "generated groups mismatch";
+    }
+
+    console.log(groupPaths);
+
+
+
+
+    // --- for each group, generating the item paths for items in the group
+    const newItemPaths:string[][]=_.map(items,(innerItems:string[],itemsI:number):string[]=>{
+        const groupPath:string=groupPaths[itemsI];
+
+        // generate new item names with the renamer
+        var newItemNames:string[]=renamer.compileRule(innerItems,itemRenameRule);
+
+        // add group path to the new item name to make it into actual path
+        newItemNames=_.map(newItemNames,(newItemName:string):string=>{
+            return join(groupPath,newItemName);
+        });
+
+        // check for duplicates
+        if (_.uniq(newItemNames).length!=newItemNames.length)
+        {
+            console.error("failed to rename items: duplicate item names generated");
+            throw "duplicate item names";
+        }
+
+        return newItemNames;
+    });
+
+    console.log(newItemPaths);
 }
--- tests/renamer.test.ts
+++ tests/renamer.test.ts
@@ -1,5 +1,61 @@
 import {test} from "vitest";
 
-import {test_applyRenameRule} from "@/lib/renamer";
+import {RenamerHandlebars} from "@/lib//RenamerHandlebars";
+import {renameGroupedItems} from "@/lib/renamer";
 
-test.only("apply-rename-rule",test_applyRenameRule);
+test("using RenamerHandlebars class",()=>{
+    const paths:string[]=[
+        "a.png",
+        "asdas.jpg",
+        "huh",
+        "something.txt",
+        "something.jpg",
+    ];
+
+    const renamer:RenamerHandlebars=new RenamerHandlebars();
+
+    var output:string[]=renamer.compileRule(
+        paths,
+        "{{inc 1}}",
+    );
+
+    console.log(output);
+
+    output=renamer.compileRule(
+        paths,
+        "{{inc 12}}-{{random}}",
+    );
+
+    console.log(output);
+
+    output=renamer.compileRule(
+        paths,
+        "{{random}} {{filename}}",
+    );
+
+    console.log(output);
+});
+
+test.only("main rename grouped items func",()=>{
+    const paths:string[][]=[
+        [
+            "C:/Users/ktkm/Pictures/day road and bridge.png",
+            "C:/Users/ktkm/Pictures/mall.jpg",
+        ],
+        [
+            "C:/Users/ktkm/Desktop/thing",
+            "C:/Users/ktkm/Desktop/somewhere/something.txt",
+        ]
+    ];
+
+    var groupRule:string="{{inc}}";
+    var itemRule:string="{{inc}}";
+
+    renameGroupedItems(
+        paths,
+        groupRule,
+        itemRule,
+        "C:/Users/ktkm/Desktop/thing/out",
+        "move",
+    );
+});
