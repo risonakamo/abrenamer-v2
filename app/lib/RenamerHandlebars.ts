a6048663e79edfbb24a85ae30d5f93ace9a7c283
risonakamo <hmykanae@gmail.com>
2024-09-02 11:54:34 -0400

    redesign to use class based renamer

--- app/lib/RenamerHandlebars.ts
+++ app/lib/RenamerHandlebars.ts
@@ -1,6 +1,84 @@
-// class wrapping around handlebars env to implement custom renaming functions
+// class wrapping around handlebars env to implement custom renaming functions.
+// this class provides function for renaming a list files into new names using
+// the rename rule. it only renames the basename of the path, and drops the rest
+// of the path before the basename.
 
-class RenamerHandlebars
+import {create} from "handlebars"
+import _ from "lodash";
+import {v4 as uuid4} from "uuid";
+import {extname,parse,ParsedPath} from "path";
+
+export class RenamerHandlebars
 {
+    handleBarsEnv:typeof Handlebars
 
+    firstIncCall:boolean
+    incCounter:number
+
+    currentFilename:string
+
+    constructor()
+    {
+        this.handleBarsEnv=create();
+        this.firstIncCall=true;
+        this.incCounter=0;
+        this.currentFilename="";
+
+        // inserts a number that increases each time this func is used
+        // can use startPos to set the 1st number, but does nothing
+        // after the 1st number.
+        this.handleBarsEnv.registerHelper("inc",(startPos:number|Object):number=>{
+            var startPos2:number=1;
+
+            if (_.isNumber(startPos))
+            {
+                startPos2=startPos;
+            }
+
+            if (this.firstIncCall)
+            {
+                this.incCounter=startPos2-1;
+            }
+
+            this.firstIncCall=false;
+
+            this.incCounter++;
+            return this.incCounter;
+        });
+
+        // inserts random hash
+        this.handleBarsEnv.registerHelper("random",():string=>{
+            return uuid4().slice(0,7);
+        });
+
+        // inserts original filename
+        this.handleBarsEnv.registerHelper("filename",():string=>{
+            return this.currentFilename;
+        });
+    }
+
+    /** reset the persistent vars used during each compilation call */
+    resetVars():void
+    {
+        this.firstIncCall=true;
+        this.incCounter=0;
+        this.currentFilename="";
+    }
+
+    /** apply the rule to list of filepaths */
+    compileRule(filepaths:string[],rule:string):string[]
+    {
+        this.resetVars();
+
+        const renamerTemplate:HandlebarsTemplateDelegate<{}>=this.handleBarsEnv.compile(rule);
+
+        return _.map(filepaths,(filepath:string):string=>{
+            const parsed:ParsedPath=parse(filepath);
+
+            const extension:string=parsed.ext;
+            this.currentFilename=parsed.name;
+
+            return `${renamerTemplate({})}${extension}`;
+        });
+    }
 }
--- app/lib/renamer.ts
+++ app/lib/renamer.ts
@@ -1,9 +1,8 @@
 // renamer lib funcs
 
-import Handlebars,{create} from "handlebars";
-import {basename,extname} from "path";
 import _ from "lodash";
-import {v4 as uuid4} from "uuid";
+
+import {RenamerHandlebars} from "@/lib/RenamerHandlebars";
 
 function renameGroupedItems(
     items:GroupedPaths,
@@ -16,67 +15,6 @@ function renameGroupedItems(
 
 }
 
-/** apply rename rule to list of file paths. only the basename is considered and produced, the
- *  rest of the filepath (before the basename) is not used. refer to rename rule system doc
- *  for how rename rule works */
-function applyRenameRule(
-    filepaths:string[],
-    renameRule:string,
-):string[]
-{
-    const renamerTemplate:HandlebarsTemplateDelegate<{}>=createRenamerEnv(renameRule);
-
-    return _.map(filepaths,(filepath:string):string=>{
-        const extension:string=extname(filepath);
-
-        return `${renamerTemplate({})}${extension}`;
-    });
-}
-
-/** creates handlebars template to perform renaming using given rename rule. refer to rename
- *  rule document for rename rule usage */
-function createRenamerEnv(renamerRule:string):HandlebarsTemplateDelegate<{}>
-{
-    const handleBarsEnv:typeof Handlebars=create();
-
-    var firstIncCall:boolean=true;
-    var incCounter:number=0;
-
-    // inserts a number that increases each time this func is used
-    // can use startPos to set the 1st number, but does nothing
-    // after the 1st number.
-    handleBarsEnv.registerHelper("inc",(startPos:number|Object):number=>{
-        var startPos2:number=1;
-
-        if (_.isNumber(startPos))
-        {
-            startPos2=startPos;
-        }
-
-        if (firstIncCall)
-        {
-            incCounter=startPos2-1;
-        }
-
-        firstIncCall=false;
-
-        incCounter++;
-        return incCounter;
-    });
-
-    // inserts random hash
-    handleBarsEnv.registerHelper("random",():string=>{
-        return uuid4().slice(0,7);
-    });
-
-    // inserts original filename
-    handleBarsEnv.registerHelper("filename",():string=>{
-
-    });
-
-    return handleBarsEnv.compile(renamerRule);
-}
-
 export function test_applyRenameRule():void
 {
     const paths:string[]=[
@@ -87,16 +25,25 @@ export function test_applyRenameRule():void
         "something.jpg",
     ];
 
-    var output:string[]=applyRenameRule(
+    const renamer:RenamerHandlebars=new RenamerHandlebars();
+
+    var output:string[]=renamer.compileRule(
         paths,
         "{{inc 1}}",
     );
 
     console.log(output);
 
-    var output=applyRenameRule(
+    output=renamer.compileRule(
         paths,
         "{{inc 12}}-{{random}}",
+    );
+
+    console.log(output);
+
+    output=renamer.compileRule(
+        paths,
+        "{{random}} {{filename}}",
     );
 
     console.log(output);
